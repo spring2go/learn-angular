a0ecfa1346814e8b9dbb7bf8d0eeb0c7eb3370c3
bobo <bobo@spring2go.com>
2021-03-14 22:17:14 +0800

    add 1.8 nesting components and input

---
+++ 1.hello-angular/8.nesting-components-and-input/src/main.ts
@@ -0,0 +1,73 @@
+import { Component, Input, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+class Poem {
+  public poemLine: string;
+  public nameWithAuthor: string;
+  public hide: boolean;
+
+  constructor(poemLine: string, nameWithAuthor: string) {
+    this.poemLine = poemLine;
+    this.nameWithAuthor = nameWithAuthor;
+    this.hide = true;
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+  }
+}
+
+@Component({
+  selector: "poem",
+  template: `
+    <div class="card card-block">
+      <h4 class="card-title">{{ data.poemLine }}</h4>
+      <p class="card-text" [hidden]="data.hide">{{ data.nameWithAuthor }}</p>
+      <a (click)="data.toggle()" class="btn btn-warning">告诉我 </a>
+    </div>
+  `,
+})
+class PoemComponent {
+  @Input("poem") data: Poem;
+}
+
+@Component({
+  selector: "poem-list",
+  template: ` <poem *ngFor="let p of poems" [poem]="p"></poem> `,
+})
+class PoemListComponent {
+  poems: Poem[]; // Array<Object>
+
+  constructor() {
+    this.poems = [
+      new Poem(
+        "好雨知时节，当春乃发生，随风潜入夜，润物细无声。",
+        "春夜喜雨(杜甫)"
+      ),
+      new Poem(
+        "床前明月光，疑是地上霜。举头望明月，低头思故乡。",
+        "静夜思(李白)"
+      ),
+      new Poem(
+        "月落乌啼霜满天，江枫渔火对愁眠。姑苏城外寒山寺，夜半钟声到客船。",
+        "枫桥夜泊(张继)"
+      ),
+    ];
+  }
+}
+
+@Component({
+  selector: "app",
+  template: ` <poem-list></poem-list> `,
+})
+class AppComponent {}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, PoemComponent, PoemListComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
